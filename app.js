9dbf1c909457224513de1dac29027d3aefc9cb30
sundarasekar <sundarasekar01@gmail.com>
2023-08-10 18:21:58 +0530

    its working

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+const fs = require("fs");
 
 
 app.get("/", (req, res) => {
@@ -59,6 +60,27 @@ app.get("/show", async (req, res) => {
     res.sendFile(imagePath);
   });
 
+  app.get("/delete/:imageName", async (req, res) => {
+    try {
+      const imageName = req.params.imageName;
+      const galleryData = await gallery.findOneAndUpdate(
+        {},
+        { $pull: { image: imageName } },
+        { new: true }
+      );
+      
+      if (galleryData) {
+        const imagePath = path.join(__dirname,"gallery", imageName);
+        fs.unlinkSync(imagePath); // Delete the image file from the server
+        res.redirect("/show");
+      } else {
+        res.redirect("/show");
+      }
+    } catch (error) {
+      console.log(error.message);
+      res.redirect("/show");
+    }
+  });
 
 app.listen(1000, () => {
   console.log("Server is running on port 1000");
